3c0bba87e96bb252e674d28aa762d9ac132d2e74
char@odin <char3176@gmail.com>
2026-01-19 22:10:56 -0500

    Update to WPILIB 20256.2.1, REVLib 2026, photonlib 2026.1.1-rc-3

--- src/main/java/com/team3176/robot/BuildConstants.java
+++ src/main/java/com/team3176/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "75a575a3b46004ac68641cab523dcaa0d1d608db";
-  public static final String GIT_DATE = "2026-01-11 21:56:57 EST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "53c2f65c04bf681be66924e8285813b0705e4d3b";
+  public static final String GIT_DATE = "2026-01-12 20:50:03 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-11 21:59:19 EST";
-  public static final long BUILD_UNIX_TIME = 1768186759190L;
+  public static final String BUILD_DATE = "2026-01-19 22:06:22 EST";
+  public static final long BUILD_UNIX_TIME = 1768878382312L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
