5bc6c3a3026aed637de9516c9835b3d804dc09ef
Liam McNeely <lpmcneely@outlook.com>
2026-01-22 18:51:29 -0500

    getting ready to branch

--- src/main/java/com/team3176/robot/BuildConstants.java
+++ src/main/java/com/team3176/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "75a575a3b46004ac68641cab523dcaa0d1d608db";
-  public static final String GIT_DATE = "2026-01-11 21:56:57 EST";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "d36442b8973cc3540912fe15765a74b7cd3a8885";
+  public static final String GIT_DATE = "2026-01-11 22:12:54 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-11 21:59:19 EST";
-  public static final long BUILD_UNIX_TIME = 1768186759190L;
+  public static final String BUILD_DATE = "2026-01-19 17:04:46 EST";
+  public static final long BUILD_UNIX_TIME = 1768860286340L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
