a4f072d3a35f3da2b797389bf6b0ed370456068f
MooseRobot@LangeLenovo <James.R.Lange@gmail.com>
2026-01-29 22:24:23 -0500

    Updated Constants

--- src/main/java/com/team3176/robot/BuildConstants.java
+++ src/main/java/com/team3176/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "f0162a0d51d50e13aaad5533efb20efc7b24ba51";
-  public static final String GIT_DATE = "2026-01-29 19:43:56 EST";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "a093c26cc2c60289f38125df69f4039c432638a0";
+  public static final String GIT_DATE = "2026-01-29 22:22:21 EST";
   public static final String GIT_BRANCH = "GenericTalon";
-  public static final String BUILD_DATE = "2026-01-29 21:52:12 EST";
-  public static final long BUILD_UNIX_TIME = 1769741532733L;
+  public static final String BUILD_DATE = "2026-01-29 22:22:34 EST";
+  public static final long BUILD_UNIX_TIME = 1769743354936L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/com/team3176/robot/constants/SuperStructureConstants.java
+++ src/main/java/com/team3176/robot/constants/SuperStructureConstants.java
@@ -3,11 +3,12 @@ package com.team3176.robot.constants;
 public class SuperStructureConstants {
 
 
-  public static final double GenericTalon_OUTPUT_POWER = 1;
-  public static final int    GenericTalon_CURRENT_LIMIT_A = 15;
-  public static final double GenericTalon_kP = 1;
+  public static final double GenericTalon_MAX_OUTPUT_VOLTS = 2.5; //Sets Max forward Speed
+  public static final double GenericTalon_MAXNeg_OUTPUT_VOLTS = -2.5; //Sets Max Reverse Speed
+   public static final double GenericTalon_kP = 1;
   public static final double GenericTalon_kI = 0; 
   public static final double GenericTalon_kD = 0; 
+  
   //Set if using gravity for feedforward
   public static final double GenericTalon_kg = 0.2;
 
