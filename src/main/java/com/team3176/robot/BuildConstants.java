98e9bca56f5e2cdd2977925f8d75ba4fe55521f6
Jeremiah Parker (Jpark) <jerepark08@gmail.com>
2026-02-02 19:44:57 -0500

    position motor only moves now
    all of the speed controls are removed

--- src/main/java/com/team3176/robot/BuildConstants.java
+++ src/main/java/com/team3176/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "269516f7f9cd0f02a78bc9a0f512381bea49a1da";
-  public static final String GIT_DATE = "2026-01-31 13:22:00 EST";
-  public static final String GIT_BRANCH = "GenericTalon";
-  public static final String BUILD_DATE = "2026-01-31 15:05:55 EST";
-  public static final long BUILD_UNIX_TIME = 1769889955908L;
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "736d7fea91ca97adea62ad9e8b0659bf47899f2d";
+  public static final String GIT_DATE = "2026-01-31 15:17:07 EST";
+  public static final String GIT_BRANCH = "hood";
+  public static final String BUILD_DATE = "2026-02-02 19:39:26 EST";
+  public static final long BUILD_UNIX_TIME = 1770079166716L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/com/team3176/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/com/team3176/robot/subsystems/superstructure/Superstructure.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import java.util.function.DoubleSupplier;
+
+import com.team3176.robot.subsystems.HoodControl.Hood;
 //import com.team3176.robot.constants.FieldConstants;
 // import java.util.function.IntSupplier;
 import com.team3176.robot.subsystems.superstructure.GenericTalonControl.GenericTalon;
@@ -17,14 +19,21 @@ public class Superstructure {
   private static Superstructure instance;
 
   private GenericTalon genericTalon;
+  private Hood hood;
 
   public Superstructure() {
 
     genericTalon = GenericTalon.getInstance();
+
+    hood = hood.getInstance();
   }
 
   public Command genericPositionMotor(DoubleSupplier position) {
     return (genericTalon.runGenericTalon(() -> position.getAsDouble()));
+  }
+
+    public Command HoodMotor(DoubleSupplier position) {
+    return (hood.runHood(() -> position.getAsDouble()));
   }
 
     public Command genericMotorSpeed(DoubleSupplier Speed_RPS) {
