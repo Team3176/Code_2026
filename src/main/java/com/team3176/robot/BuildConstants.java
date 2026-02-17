2b5e781ab584b94403162149021a7cd60521309d
Prabahar Ignatius <brian.ignesh@gmail.com>
2026-02-16 20:35:50 -0500

    Adding our Initial files to our branch on Feb 16

--- src/main/java/com/team3176/robot/BuildConstants.java
+++ src/main/java/com/team3176/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "9260524ab39a712b1b9f663e8993b482d2ea5911";
-  public static final String GIT_DATE = "2026-02-05 13:08:24 EST";
-  public static final String GIT_BRANCH = "GenericTalon";
-  public static final String BUILD_DATE = "2026-02-05 13:20:04 EST";
-  public static final long BUILD_UNIX_TIME = 1770315604032L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "7853d12222ec7ed12a34035b8a163022c0984ae1";
+  public static final String GIT_DATE = "2026-02-05 22:12:55 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-16 20:28:55 EST";
+  public static final long BUILD_UNIX_TIME = 1771291735245L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/precisionPigeon.java
@@ -0,0 +1,49 @@
+package frc.robot;
+
+import edu.wpi.first.networktables.BooleanArrayPublisher;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.IntegerArrayPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import com.ctre.phoenix6.hardware.Pigeon2;;
+
+public class precisionPigeon {
+
+    private static int pigeonCanID = 27; //This is the can ID of the IMU set in phoenix tuner
+    private static String pigeonCANBusLocation = "rio"; //this is the canbus that the pigeon is installed on (note this method to be deprecated in 2027)
+    private Pigeon2 aPigeonIMU;
+    private NetworkTable IMUDataNT;
+    private DoubleArrayPublisher accelerationsDisp;
+    private DoubleArrayPublisher velocitiesDisp;
+    private DoubleArrayPublisher positionsDisp;
+    private DoubleArrayPublisher rotationalAccelsDisp;
+    private DoubleArrayPublisher rotationalVelocitiesDisp;
+    private DoubleArrayPublisher rotationalYPRDisp;
+
+
+    public precisionPigeon(){
+        aPigeonIMU = new Pigeon2(pigeonCanID,pigeonCANBusLocation);
+
+        IMUDataNT = NetworkTableInstance.getDefault().getTable("IMU Data");
+        accelerationsDisp           = IMUDataNT.getDoubleArrayTopic("Accels").publish();
+        velocitiesDisp              = IMUDataNT.getDoubleArrayTopic("Velocities").publish();
+        positionsDisp               = IMUDataNT.getDoubleArrayTopic("positions").publish();
+        rotationalAccelsDisp        = IMUDataNT.getDoubleArrayTopic("RotAccel").publish();
+        rotationalVelocitiesDisp    = IMUDataNT.getDoubleArrayTopic("RotVel").publish();
+        rotationalYPRDisp           = IMUDataNT.getDoubleArrayTopic("RotPos").publish();
+
+    }
+
+    public double PeriodicUpdate(){
+        double yaw = aPigeonIMU.getYaw().getValueAsDouble();
+
+        double[] ypr =  {yaw,0,0};
+        rotationalVelocitiesDisp.set(ypr);
+
+        return yaw;
+    }
+
+}
