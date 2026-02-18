334acec11accf2d71f1e6be16ce7df20a45e5bf2
dmckenn2 <46730808+dmckenn2@users.noreply.github.com>
2026-02-17 23:21:11 -0500

    Untested code to read a yaw from elastic and update it on a toggle.

--- src/main/java/frc/robot/precisionPigeon.java
+++ src/main/java/frc/robot/precisionPigeon.java
@@ -2,13 +2,23 @@ package frc.robot;
 
 import edu.wpi.first.networktables.BooleanArrayPublisher;
 import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.BooleanEntry;
+import edu.wpi.first.networktables.BooleanSubscriber;
+import edu.wpi.first.networktables.DoubleArrayEntry;
 import edu.wpi.first.networktables.DoubleArrayPublisher;
+import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.IntegerArrayPublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOptions;
 
-import com.ctre.phoenix6.hardware.Pigeon2;;
+import javax.sound.sampled.BooleanControl;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import dev.doglog.internal.tunable.entry.ToggleableBooleanSubscriber;;
 
 public class precisionPigeon {
 
@@ -22,6 +32,10 @@ public class precisionPigeon {
     private DoubleArrayPublisher rotationalAccelsDisp;
     private DoubleArrayPublisher rotationalVelocitiesDisp;
     private DoubleArrayPublisher rotationalYPRDisp;
+    private DoubleSubscriber manualX;
+    private DoubleSubscriber manualY;
+    private DoubleSubscriber manualYaw;
+    private BooleanSubscriber updatePosition;
 
 
     public precisionPigeon(){
@@ -35,15 +49,39 @@ public class precisionPigeon {
         rotationalVelocitiesDisp    = IMUDataNT.getDoubleArrayTopic("RotVel").publish();
         rotationalYPRDisp           = IMUDataNT.getDoubleArrayTopic("RotPos").publish();
 
+        manualX                     =IMUDataNT.getDoubleTopic("ManualX").subscribe(0);
+        manualY                     =IMUDataNT.getDoubleTopic("ManualY").subscribe(0);
+        manualYaw                   =IMUDataNT.getDoubleTopic("ManualY").subscribe(0);
+        updatePosition              =IMUDataNT.getBooleanTopic("UpdatePosition").subscribe(false);
+            
+        
+
     }
 
     public double PeriodicUpdate(){
         double yaw = aPigeonIMU.getYaw().getValueAsDouble();
+        double pitch = aPigeonIMU.getPitch().getValueAsDouble();
+        double roll = aPigeonIMU.getRoll().getValueAsDouble();
 
-        double[] ypr =  {yaw,0,0};
+        double[] ypr =  {yaw,pitch,roll};
         positionsDisp.set(ypr);
 
         return yaw;
+    }
+
+    public void updateCurrentPositionEstimate(double x, double y, double yaw){
+        
+        aPigeonIMU.setYaw(yaw);
+
+    }
+
+    private void checkForPositionInput(){
+        if(updatePosition.getAsBoolean()){
+
+            aPigeonIMU.setYaw(manualYaw.get());
+
+        }
+
     }
 
 }
