8a933b21850a584580765e71aa4e77c47f525caf
penguin <penguinpenguinpenguin3278@gmail.com>
2026-03-01 00:07:46 -0800

    shoot sequence
    part 3
    not complete

--- src/main/java/com/team3176/robot/BuildConstants.java
+++ src/main/java/com/team3176/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 90;
-  public static final String GIT_SHA = "fe7352d1072f266545b92cde6f911f1d8e458840";
-  public static final String GIT_DATE = "2026-02-28 11:59:08 EST";
+  public static final int GIT_REVISION = 91;
+  public static final String GIT_SHA = "9094cbeb3a2245aca2bdc5c98245c1f622425729";
+  public static final String GIT_DATE = "2026-03-01 01:25:04 EST";
   public static final String GIT_BRANCH = "ProductionIntegrationBranch";
-  public static final String BUILD_DATE = "2026-03-01 01:23:44 EST";
-  public static final long BUILD_UNIX_TIME = 1772346224965L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-01 01:34:34 EST";
+  public static final long BUILD_UNIX_TIME = 1772346874763L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
