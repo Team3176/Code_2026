170c83750f8e2a3a23007b2d7988a4857d62410e
Grant3176 <grantabutts@icloud.com>
2026-03-12 20:50:35 -0400

    fixed and added autons
     Layton's work

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 66;
-  public static final String GIT_SHA = "2841eb1901e797abe405a4982877f4724afcb9e9";
-  public static final String GIT_DATE = "2026-03-08 10:26:02 EDT";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "d47b4300eed2a6ff03adfc752a7849501e5023c4";
+  public static final String GIT_DATE = "2026-03-12 13:38:14 EDT";
   public static final String GIT_BRANCH = "MishProduction";
-  public static final String BUILD_DATE = "2026-03-08 14:46:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1772995602637L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-12 19:15:14 EDT";
+  public static final long BUILD_UNIX_TIME = 1773357314251L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
