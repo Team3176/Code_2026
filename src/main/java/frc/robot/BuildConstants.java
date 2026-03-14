380ba6cb0090a853ad949d3d7edde48e0cc46254
dmckenn2 <46730808+dmckenn2@users.noreply.github.com>
2026-03-14 11:05:39 -0400

    field estimation working

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 69;
-  public static final String GIT_SHA = "a7e83a27584d729ef407ee751d911bc3272e7943";
-  public static final String GIT_DATE = "2026-03-13 20:12:34 EDT";
+  public static final int GIT_REVISION = 70;
+  public static final String GIT_SHA = "5709cb17afac7a6325546e1907f236b5ef4938e7";
+  public static final String GIT_DATE = "2026-03-13 20:21:29 EDT";
   public static final String GIT_BRANCH = "postMishVisionDev";
-  public static final String BUILD_DATE = "2026-03-13 20:19:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1773447586347L;
+  public static final String BUILD_DATE = "2026-03-14 10:32:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1773498770923L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
