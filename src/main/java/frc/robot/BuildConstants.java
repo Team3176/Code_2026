dc18d341e1c6be7a8c7ad5be884363a80bdbe649
MooseRobot@LangeLenovo <James.R.Lange@gmail.com>
2026-04-14 18:28:22 -0400

    Auto stash before merge of "PriortoState" and "origin/PriortoState"

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 152;
-  public static final String GIT_SHA = "16f5d777264893783f7df30e7bcca00c07a2b116";
-  public static final String GIT_DATE = "2026-04-11 15:03:28 EDT";
+  public static final int GIT_REVISION = 153;
+  public static final String GIT_SHA = "66d595a119c49a7538f2bbff087dc6b61e57cd19";
+  public static final String GIT_DATE = "2026-04-14 18:27:55 EDT";
   public static final String GIT_BRANCH = "PriortoState";
-  public static final String BUILD_DATE = "2026-04-11 16:44:31 EDT";
-  public static final long BUILD_UNIX_TIME = 1775940271987L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-04-14 18:27:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1776205679986L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
