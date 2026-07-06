6318ff060e747de8a4a816166b7d709acad6858a
Kai R. (3176B) <team3176b@gmail.com>
2026-07-06 19:33:51 -0400

    Red Alert Pre-IRI
    New Autos for IRI

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code_2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 171;
-  public static final String GIT_SHA = "8f8ce6ea2887db2a291813314feb294ad2b52bca";
-  public static final String GIT_DATE = "2026-04-19 10:00:05 EDT";
+  public static final int GIT_REVISION = 172;
+  public static final String GIT_SHA = "7e094cabe14a5f750d97a06f42d0753e99beb297";
+  public static final String GIT_DATE = "2026-04-19 10:29:04 EDT";
   public static final String GIT_BRANCH = "PriortoState";
-  public static final String BUILD_DATE = "2026-04-19 10:07:52 EDT";
-  public static final long BUILD_UNIX_TIME = 1776607672330L;
+  public static final String BUILD_DATE = "2026-07-06 19:30:38 EDT";
+  public static final long BUILD_UNIX_TIME = 1783380638545L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
